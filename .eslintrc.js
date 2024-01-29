60f998eddf134436df2883edc666cc28f1587912
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2024-01-29 23:41:23 +0900

    build: initial setting

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "no-nested-ternary": 0
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+};
